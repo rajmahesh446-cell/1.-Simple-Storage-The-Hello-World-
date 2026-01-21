d93075c676377c31ea0d07d9de5322c2d7eb5710
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-01-21 15:28:44 +0530

    Create simplestorage.sol

---
+++ simplestorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private data;
+
+    // Function to update the value of data
+    function set(uint256 _x) public {
+        data = _x;
+    }
+
+    // Function to read the value of data
+    function get() public view returns (uint256) {
+        return data;
+    }
+}
